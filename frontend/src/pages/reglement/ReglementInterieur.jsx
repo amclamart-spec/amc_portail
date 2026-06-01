85ac566acc542f58291523804e2676730d028418
GHANDRI <amclamart@gmail.com>
2026-06-02 01:17:09 +0200

    fix5

--- frontend/src/pages/reglement/ReglementInterieur.jsx
+++ frontend/src/pages/reglement/ReglementInterieur.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 import toast from 'react-hot-toast';
-import api from '../api/axios';
+import api from '../../api/axios';
 
 export default function ReglementInterieur() {
   const handleClose = () => {
