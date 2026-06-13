b6a84d4754c82dc7aa9cdbaf1e2a841482cbc5a4
GHANDRI <amclamart@gmail.com>
2026-06-13 23:30:43 +0200

    fix et evols

--- frontend/src/utils/roles.js
+++ frontend/src/utils/roles.js
@@ -1,9 +1,13 @@
 export const ROLE_HOME = {
-  SUPER_ADMIN: '/super-admin',
+  SUPER_ADMIN: '/admin',
   ADMIN: '/admin',
   TRESORIER: '/tresorier',
   PROFESSEUR: '/professeur',
   FAMILLE: '/famille',
+  RESPONSABLE_POLE_CORAN:      '/admin',
+  RESPONSABLE_POLE_ARABE:      '/admin',
+  RESPONSABLE_POLE_SOUTIEN_SCO: '/admin',
+  RESPONSABLE_POLE_SCIENCE_IS:  '/admin',
 };
 
 export const ROLE_LABEL = {
@@ -12,7 +16,18 @@ export const ROLE_LABEL = {
   TRESORIER: 'Trésorerie',
   PROFESSEUR: 'Espace Professeur',
   FAMILLE: 'Espace Famille',
+  RESPONSABLE_POLE_CORAN:       'Responsable Pôle Coran',
+  RESPONSABLE_POLE_ARABE:       'Responsable Pôle Arabe',
+  RESPONSABLE_POLE_SOUTIEN_SCO: 'Responsable Pôle Soutien Scolaire',
+  RESPONSABLE_POLE_SCIENCE_IS:  'Responsable Pôle Sciences Islamiques',
 };
+
+export const RESPONSABLE_POLE_ROLES = [
+  'RESPONSABLE_POLE_CORAN',
+  'RESPONSABLE_POLE_ARABE',
+  'RESPONSABLE_POLE_SOUTIEN_SCO',
+  'RESPONSABLE_POLE_SCIENCE_IS',
+];
 
 export function getHomeForRole(role) {
   return ROLE_HOME[role] || '/login';
